d25a35a8693a5221a2e76c6ac85dc865871d559a
CodeInBars <ismaclaseifc3@gmail.com>
2020-02-05 13:35:50 +0100

    Servlet

---
+++ src/controlador/ControladorServlet.java
@@ -0,0 +1,54 @@
+package controlador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class ControladorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public ControladorServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<HTML>");
+		out.println("<HEAD><TITLE>Formulario Alumno</TITLE></HEAD>");
+		out.println("<BODY>");
+		out.println("<table><tr><td>Nombre</td><td>Apellido</td><td>Curso</td></tr>");
+		out.println("<tr><td><input type='text' value='nombre'></td>");
+		out.println("<td><input type='text' value='apellido'></td>");
+		out.println("<td> <select>\n" + 
+				"  <option value='1DAM'>1DAM</option>\n" + 
+				"  <option value='2DAM'>2DAM</option>\n" + 
+				"</select> </td></tr>");
+		out.println("</table>");
+		
+		out.println("<br>");
+		out.println("</BODY></HTML>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
