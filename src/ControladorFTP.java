222f1f0afc087a26056b3cc685118eceb8262e4f
lupoxanesp8266 <lupo.electron@gmail.com>
2020-02-10 14:21:59 +0100

    web status not ok

---
+++ src/ControladorFTP.java
@@ -0,0 +1,62 @@
+import java.io.IOException;
+import java.net.SocketException;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+
+public class ControladorFTP {
+
+	public ControladorFTP(String direc) throws SocketException, IOException {
+		super();
+		// TODO Auto-generated constructor stub
+		FTPClient cliente = new FTPClient();
+
+		String servFTP = "192.168.2.35";
+
+		System.out.println("Nos conectamos a: " + servFTP);
+		String usuario = "prudensito";
+		String clave = "1234";
+
+		cliente.connect(servFTP);
+		boolean login = cliente.login(usuario, clave);
+
+		if (login) {
+			System.out.println("Login correcto...");
+		} else {
+
+			System.out.println("Login incorrecto");
+			cliente.disconnect();
+			System.exit(1);
+		}
+		cliente.changeWorkingDirectory("files");
+		System.out.println("Directorio actual:" + cliente.printWorkingDirectory());
+
+		FTPFile[] files = cliente.listFiles();
+		System.out.println("Ficheros en el directorio actual:" + files.length);
+
+		String tipos[] = { "Fichero", "Directorio", "Enlace simb." };
+
+		for (int i = 0; i < files.length; i++) {
+			System.out.println("\t" + files[i].getName() + "=>" + tipos[files[i].getType()]);
+		}
+
+		if (cliente.makeDirectory(direc)) {
+			System.out.println("Directorio creado");
+			cliente.changeWorkingDirectory(direc);
+		} else {
+			System.out.println("NO SE HA PODIDO CREAR DIRECTORIO");
+		}
+
+		boolean logout = cliente.logout();
+		if (logout) {
+			System.out.println("Logout del servidor FTP...");
+		} else {
+			System.out.println("Error al hacer Logout...");
+		}
+
+		cliente.disconnect();
+
+		System.out.println("Conexi�n finalizada");
+	}
+
+}
--- src/ServletFtp.java
+++ src/ServletFtp.java
@@ -1,21 +1,23 @@
 
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-public class ControladorServlet extends HttpServlet {
+/**
+ * Servlet implementation class ServletFtp
+ */
+@WebServlet("/ServletFtp")
+public class ServletFtp extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static ArrayList<Persona> persona = new ArrayList<Persona>();
-
-    public ControladorServlet() {
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletFtp() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -24,24 +26,7 @@ public class ControladorServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		
-		String nombre = request.getParameter("nombre");
-		String apellido = request.getParameter("apellido");
-		String curso = request.getParameter("curso");
-		
-		Persona p = new Persona(nombre,apellido,curso);
-		
-		persona.add(p);
-		out.println("<table><tr><td>Nombre</td><td>Apellido</td><td>Curso</td></tr>");
-		for(Persona pers : persona) {
-			out.println("<tr><td>" + pers.getNombre() + "</td><td>" + pers.getApellido() +"</td><td>" + pers.getCurso() + "</td><td><input type='submit' value = 'Crear Directorio'></td></tr>");
-		}
-		
-		out.println("</table>");
-		
+		ControladorFTP ftp = new ControladorFTP("putavidatt");
 	}
 
 	/**
