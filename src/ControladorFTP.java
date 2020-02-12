7342543765c73a443e28a7eff1d35f8959300ac8
lupoxanesp8266 <lupo.electron@gmail.com>
2020-02-12 13:48:26 +0100

    acho1

--- src/ControladorFTP.java
+++ src/ControladorFTP.java
@@ -6,8 +6,7 @@ import org.apache.commons.net.ftp.FTPFile;
 
 public class ControladorFTP {
 
-	public ControladorFTP(String direc) throws SocketException, IOException {
-		super();
+	public void crearDir(String direc) {
 		// TODO Auto-generated constructor stub
 		FTPClient cliente = new FTPClient();
 
@@ -17,46 +16,56 @@ public class ControladorFTP {
 		String usuario = "prudensito";
 		String clave = "1234";
 
-		cliente.connect(servFTP);
-		boolean login = cliente.login(usuario, clave);
+		try {
+			cliente.connect(servFTP);
+			boolean login = cliente.login(usuario, clave);
 
-		if (login) {
-			System.out.println("Login correcto...");
-		} else {
+			if (login) {
+				System.out.println("Login correcto...");
+			} else {
 
-			System.out.println("Login incorrecto");
+				System.out.println("Login incorrecto");
+				cliente.disconnect();
+				System.exit(1);
+			}
+			cliente.changeWorkingDirectory("files");
+			System.out.println("Directorio actual:" + cliente.printWorkingDirectory());
+
+			FTPFile[] files = cliente.listFiles();
+			System.out.println("Ficheros en el directorio actual:" + files.length);
+
+			String tipos[] = { "Fichero", "Directorio", "Enlace simb." };
+
+			for (int i = 0; i < files.length; i++) {
+				System.out.println("\t" + files[i].getName() + "=>" + tipos[files[i].getType()]);
+			}
+
+			if (cliente.makeDirectory(direc)) {
+				System.out.println("Directorio creado");
+				cliente.changeWorkingDirectory(direc);
+			} else {
+				System.out.println("NO SE HA PODIDO CREAR DIRECTORIO");
+			}
+
+			boolean logout = cliente.logout();
+			if (logout) {
+				System.out.println("Logout del servidor FTP...");
+			} else {
+				System.out.println("Error al hacer Logout...");
+			}
+
 			cliente.disconnect();
-			System.exit(1);
-		}
-		cliente.changeWorkingDirectory("files");
-		System.out.println("Directorio actual:" + cliente.printWorkingDirectory());
 
-		FTPFile[] files = cliente.listFiles();
-		System.out.println("Ficheros en el directorio actual:" + files.length);
-
-		String tipos[] = { "Fichero", "Directorio", "Enlace simb." };
-
-		for (int i = 0; i < files.length; i++) {
-			System.out.println("\t" + files[i].getName() + "=>" + tipos[files[i].getType()]);
+			System.out.println("Conexi�n finalizada");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
-		if (cliente.makeDirectory(direc)) {
-			System.out.println("Directorio creado");
-			cliente.changeWorkingDirectory(direc);
-		} else {
-			System.out.println("NO SE HA PODIDO CREAR DIRECTORIO");
-		}
+	}
 
-		boolean logout = cliente.logout();
-		if (logout) {
-			System.out.println("Logout del servidor FTP...");
-		} else {
-			System.out.println("Error al hacer Logout...");
-		}
-
-		cliente.disconnect();
-
-		System.out.println("Conexi�n finalizada");
+	public ControladorFTP() {
+		super();
 	}
 
 }
--- src/ServletFtp.java
+++ src/ServletFtp.java
@@ -1,6 +1,7 @@
 
-
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,28 +14,35 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ServletFtp")
 public class ServletFtp extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletFtp() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ControladorFTP ftp = new ControladorFTP("putavidatt");
+	public ServletFtp() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// ControladorFTP ftp = new ControladorFTP("putavidatt");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType( "text/html; charset=UTF-8" );
+		PrintWriter out = response.getWriter();
+		ControladorFTP ftp = new ControladorFTP();
+		ftp.crearDir("ManuIsma");
+		out.println("Hecho");
 	}
 
 }
