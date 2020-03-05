beb6e053bf8e57fcd2ad9c7aa611e3c9a1826bd9
lupoxanesp8266 <lupo.electron@gmail.com>
2020-03-05 11:48:45 +0100

    VUestra asquerosa vida

--- src/ControladorFTP.java
+++ src/ControladorFTP.java
@@ -1,37 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.SocketException;
-
+import java.io.OutputStream;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 
-import java.io.Writer;
-import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Signature;
-import java.security.UnrecoverableKeyException;
-import java.security.spec.InvalidKeySpecException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-import javax.net.ssl.KeyManager;
-import javax.net.ssl.KeyManagerFactory;
+import javax.mail.internet.MimeMultipart;
 
-import org.apache.commons.net.smtp.AuthenticatingSMTPClient;
-import org.apache.commons.net.smtp.AuthenticatingSMTPClient.AUTH_METHOD;
-import org.apache.commons.net.smtp.SMTPReply;
-import org.apache.commons.net.smtp.SimpleSMTPHeader;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfWriter;
 
 public class ControladorFTP {
 
+	private String fecha;
+	
 	public void crearDir(String direc, String email) {
 		// TODO Auto-generated constructor stub
 		FTPClient cliente = new FTPClient();
@@ -74,8 +81,6 @@ public class ControladorFTP {
 				System.out.println("\t" + files[i].getName() + "=>" + tipos[files[i].getType()]);
 			}
 
-			
-
 			boolean logout = cliente.logout();
 			if (logout) {
 				System.out.println("Logout del servidor FTP...");
@@ -93,131 +98,20 @@ public class ControladorFTP {
 
 	}
 
-	private void enviarEmail(String email) {
-		// Se crea un cliente SMTP seguro
-		AuthenticatingSMTPClient cliente = new AuthenticatingSMTPClient();
-
-		// datos del usuario y del servidor
-		String servidor = "smtp.gmail.com";
-		String usuario = "psp2dam@gmail.com"; // en este ejemplo usamos una cuenta gmail
-		String pwd = "psp.2020";
-		int puerto = 587;
-
-		int respuesta;
-		// Creación de una clave para establecer un canal serguro
-		try {
-			KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-			kmf.init(null, null);
-			KeyManager km = kmf.getKeyManagers()[0];
-			cliente.connect(servidor, puerto);
-			System.out.println("1 - " + cliente.getReplyString());
-			// se establece la clave para la comunicaci�n segura
-			cliente.setKeyManager(km);
-
-			respuesta = cliente.getReplyCode();
-			if (!SMTPReply.isPositiveCompletion(respuesta)) {
-				cliente.disconnect();
-				System.out.println("CONEXIÓN RECHAZADA");
-				System.exit(1);
-			}
-
-			// se env�a el comando EHLO
-			cliente.ehlo(servidor);// necesario
-
-			System.out.println("2 - " + cliente.getReplyString());
-
-			// Se ejecuta el comando STARTTLS y se comprueba si es true
-			if (cliente.execTLS()) {
-				System.out.println("3-" + cliente.getReplyString());
-				// se realiza la autentificaci�n con el servidor
-				if (cliente.auth(AUTH_METHOD.PLAIN, usuario, pwd)) {
-					System.out.println("4 -" + cliente.getReplyCode());
-					String destino = email;
-					String asunto = "prueba envío de email";
-					String mensaje = "Seguro que esto se env�a bien!!!";
-
-					// Se crea la cabecera
-					SimpleSMTPHeader cabecera = new SimpleSMTPHeader(usuario, destino, asunto);
-
-					// el nombre de usuario y el email de origen coinciden
-					cliente.setSender(usuario);
-					cliente.addRecipient(destino);
-					System.out.println("5 -" + cliente.getReplyString());
-
-					// Se envia DATA
-					Writer escritor = cliente.sendMessageData();
-					if (escritor == null) {
-						System.out.println("FALLO AL ENVIAR DATA");
-						System.exit(1);
-					}
-					escritor.write(cabecera.toString());
-					escritor.write(mensaje);
-					escritor.close();
-					System.out.println("6 - " + cliente.getReplyCode());
-
-					boolean exito = cliente.completePendingCommand();
-					System.out.println("7 -" + cliente.getReplyCode());
-
-					if (!exito) { // fallo
-						System.out.println("FALLO AL FINALIZAR TRANSACCI�N");
-						System.exit(1);
-					}
-
-				} else // else del m�todo auth
-					System.out.println("USUARIO NO AUTENTIFICADO");
-			} else // else del m�todo execTLS
-				System.out.println("FALLO AL EJECUTAR STARTTLS");
-
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (UnrecoverableKeyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (KeyStoreException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidKeySpecException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		try {
-			cliente.disconnect();
-		} catch (IOException e) {
-			// TODO: handle exception
-		}
-
-		System.out.println("Fin del env�o");
-		System.exit(0);
-
-	}
-
 	public ControladorFTP() {
 		super();
 	}
 
 	private void enviarEmail2(String email) {
 		
-		String servidor = "smtp.gmail.com";
 		String usuario = "psp2dam@gmail.com"; // en este ejemplo usamos una cuenta gmail
 		String pwd = "psp.2020";
 		int puerto = 587;
 		String cuerpo = "Mensaje enviardo";
+		BodyPart adjunto = new MimeBodyPart();
+		MimeMultipart multiParte = new MimeMultipart();
 		
 		try {
-			//GENERAR CLAVES
-			//Generamos el par de claves, pública y privada
-			//DSA es el método de encriptado, el algoritmo
 	
 			KeyPairGenerator k = KeyPairGenerator.getInstance("DSA");
 			k.initialize(2048); //Esto pa la longitud de la clave
@@ -249,7 +143,7 @@ public class ControladorFTP {
 	
 			props.put("mail.smtp.auth", "true");
 			props.put("mail.smtp.starttls.enable", "true");
-			props.put("mail.smtp.port", "587");
+			props.put("mail.smtp.port", puerto);
 	
 			Session session = Session.getDefaultInstance(props);
 			MimeMessage message = new MimeMessage(session);
@@ -259,6 +153,12 @@ public class ControladorFTP {
 	
 			message.setSubject("HICH");
 			message.setText(cuerpo);
+			
+			adjunto.setDataHandler(new DataHandler(new FileDataSource("/home/lubuntu/Descargas/" + fecha + ".pdf")));
+			adjunto.setFileName("file.pdf");
+			multiParte.addBodyPart(adjunto);
+			message.setContent(multiParte);
+			
 			Transport transport = session.getTransport("smtp");
 			transport.connect("smtp.gmail.com", usuario, pwd);
 			transport.sendMessage(message, message.getAllRecipients());
@@ -268,4 +168,43 @@ public class ControladorFTP {
 			me.printStackTrace();
 		}
 	}
+	
+	
+	public void imprimirFactura() {
+		OutputStream file = null;
+		Document document = null;
+		PdfWriter pdfWriter = null;
+		PdfContentByte pdfContentByte = null;
+		Timestamp fechaAhora = null;
+		fecha = null;
+
+		try {
+			fechaAhora = new Timestamp(new Date().getTime());
+			fecha = new SimpleDateFormat("yyyy_MM_dd_hh_mm_ss").format(fechaAhora);
+
+			file = new FileOutputStream(new File("/home/lubuntu/Descargas/" + fecha + ".pdf"));
+			document = new Document();
+			pdfWriter = PdfWriter.getInstance(document, file);
+
+			document.open();
+
+			pdfContentByte = pdfWriter.getDirectContent();
+
+			PdfWriter.getInstance(document, file);
+
+			document.open();
+
+			Paragraph p;
+
+			p = new Paragraph("Bienvenido",new Font(Font.FontFamily.TIMES_ROMAN, 14, Font.NORMAL));
+			p.setAlignment(Element.ALIGN_LEFT);
+			document.add((Element) p);
+
+			document.close();
+		} catch (FileNotFoundException | DocumentException ex) {
+			System.err.println(ex.getMessage());
+		}
+
+	}
+	
 }
